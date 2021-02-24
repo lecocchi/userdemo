d150355d36de79c7fc80e4a195fe0550ec91f532
= <leju1712@gmail.com>
2021-02-24 10:48:17 -0300

    Integration test

--- src/test/java/ar/com/comafi/integration/IntegrationTest.java
+++ src/test/java/ar/com/comafi/integration/IntegrationTest.java
@@ -1,18 +1,13 @@
 package ar.com.comafi.integration;
 
 import ar.com.comafi.model.User;
-import ar.com.comafi.repository.UserRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
